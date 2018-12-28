fe5dafb17afd4ec22112d6ca2bd0a32b25c49dc6
DanCardno <37637597+DanCardno@users.noreply.github.com>
2018-12-27 20:32:23 -0600

    Update installedb.sh

--- installedb.sh
+++ installedb.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# Install Prerequisites
 sudo rpm -Uvh http://yum.enterprisedb.com/edbrepos/edb-repo-latest.noarch.rpm
 sudo yum install nano wget mlocate epel-release telnet -y
 echo "******************************"
 echo "****** Enter userstring ******"
 echo "******************************"
 read userstring
-sudo sed -i "s/<username>:<password>/${userstring}/g" /etc/yum.repos.d/edb.repo
-sudo sed -i 's/enabled=0/enabled=1/g' /etc/yum.repos.d/edb.repo
+# Copy and modify repo file
+
+sudo cp /etc/yum.repos.d/edb.repo edb_backup.repo
+sudo sed -i "\/enterprisedb-dependencies/,/gpgcheck/s/enabled=0/enabled=1/" /etc/yum.repos.d/edb.repo
+sudo sed -i "\/enterprisedb-tools/,/gpgcheck/s/enabled=0/enabled=1/" /etc/yum.repos.d/edb.repo
+sudo sed -i "\/edbas11/,/gpgcheck/s/enabled=0/enabled=1/" /etc/yum.repos.d/edb.repo
 echo "******************************"
 echo "******     Patched      ******"
 echo "******************************"
+
+# Installation
 sudo yum install edb-as11 -y
 sudo /usr/edb/as11/bin/edb-as-11-setup initdb
 sudo systemctl start edb-as-11
