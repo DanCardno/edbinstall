a4dfa0e47757fb00282acca39ed19efa114e1cfc
DanCardno <37637597+DanCardno@users.noreply.github.com>
2018-12-28 13:40:16 -0600

    Create installpem.sh

---
+++ installpem.sh
@@ -0,0 +1,3 @@
+sudo setenforce 0
+yum install edb-pem-server -y
+sudo /usr/edb/pem/bin/configure-pem-server.sh -t 1 -su enterprisedb -sp Galaxy3D4d -p 5444 -ho 127.0.0.1 -ci 0.0.0.0/0 -ds edb-as-11  -acp ~/.pem/ -dbi /usr/edb/as11
