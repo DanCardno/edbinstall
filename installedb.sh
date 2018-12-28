dbd9251c2eb0b4462c2a1b84ed000ee144418945
DanCardno <37637597+DanCardno@users.noreply.github.com>
2018-12-27 19:45:15 -0600

    Update installedb.sh

--- installedb.sh
+++ installedb.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
 sudo rpm -Uvh http://yum.enterprisedb.com/edbrepos/edb-repo-latest.noarch.rpm
 sudo yum install nano wget mlocate epel-release telnet -y
+
+echo Enter userstring
+read userstring
+sudo sed -i "s/<username>:<password>/${userstring}/g" /ect/yum.repos.d/edb.repo
+sudo sed -i 's/enabled=0/enabled=1/g' /ect/yum.repos.d/edb.repo
